c87b4bdcc913a084f38a87155c8b66d972df7d4e
preetham281 <shashipreetham630@gmail.com>
2026-06-16 23:24:54 +0530

    Initial shashi app

--- shashi-app-social-run/frontend/config.js
+++ shashi-app-social-run/frontend/config.js
@@ -2,4 +2,4 @@ const isLocalBrowser = ['127.0.0.1', 'localhost'].includes(window.location.hostn
 window.SHASHI_API_BASE_URL = isLocalBrowser
   ? `http://${window.location.hostname}:5000`
   : 'https://shashi-backend.onrender.com';
-window.SHASHI_FIREBASE_VAPID_KEY = window.SHASHI_FIREBASE_VAPID_KEY || '';
+window.SHASHI_FIREBASE_VAPID_KEY = 'BFn-qvfJ0Bd1Tvvo7jkXC0qAvJdE6NVELrU7-Kumds9LMY2SugYEvOQ8fSq9XU9mnTp5U4-iIas-mtc5Kxo-v44';
